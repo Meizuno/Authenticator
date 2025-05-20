c7c1732398a00767cf2dcc72073fb215a7840457
Yurii Myronov <Yurii.Myronov@dlubal.com>
2025-05-20 14:39:42 +0200

    update code list

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="space-y-4">
+  <div class="space-y-8">
     <UInput v-model="search" placeholder="Search..." size="xl" class="w-full" />
-    <div v-for="code in searchCodes" :key="code.id">
+    <div v-for="code in searchCodes" :key="code.id" class="space-y-4">
       <ItemCard :code="code" />
+      <USeparator />
     </div>
   </div>
 </template>
