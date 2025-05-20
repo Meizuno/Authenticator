b32b195b05143a1bbe427d94f5bf010d539914a0
Yurii Myronov <Yurii.Myronov@dlubal.com>
2025-05-20 14:18:16 +0200

    add search

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <div v-for="code in codes" :key="code.id">
+  <div class="space-y-4">
+    <UInput v-model="search" placeholder="Search..." size="xl" class="w-full" />
+    <div v-for="code in searchCodes" :key="code.id">
       <ItemCard :code="code" />
     </div>
   </div>
@@ -8,7 +9,13 @@
 
 <script setup lang="ts">
 const codes = await getCodes();
-
 const { start } = useOTPState();
 start();
+
+const search = ref("");
+const searchCodes = computed(() => {
+  return codes.filter((code) => {
+    return code.name.toLowerCase().includes(search.value.toLowerCase());
+  });
+})
 </script>
