9b9d07b9c07ee55e48a84a05115d0e51c7178028
Yurii Myronov <yuramiron16@gmail.com>
2025-05-26 21:01:52 +0200

    define form component

---
+++ src/components/form.vue
@@ -0,0 +1,72 @@
+<template>
+  <UForm
+    :schema="schema"
+    :state="model"
+    class="h-full flex flex-col justify-between"
+    @submit="onSubmit"
+  >
+    <div class="space-y-4">
+      <UFormField label="Name" name="name">
+        <UInput v-model="model.name" class="w-full" />
+      </UFormField>
+
+      <UFormField label="Key" name="key">
+        <UInput
+          v-model="model.key"
+          :type="show ? 'text' : 'password'"
+          class="w-full"
+        >
+          <template #trailing>
+            <UButton
+              color="neutral"
+              variant="link"
+              size="sm"
+              :icon="show ? 'i-lucide-eye-off' : 'i-lucide-eye'"
+              :aria-label="show ? 'Hide password' : 'Show password'"
+              :aria-pressed="show"
+              aria-controls="password"
+              @click="show = !show"
+            />
+          </template>
+        </UInput>
+      </UFormField>
+
+      <UFormField label="Key Type" name="key-type">
+        <USelect
+          v-model="model.base"
+          :items="items"
+          :search-input="false"
+          class="w-full"
+        />
+      </UFormField>
+    </div>
+
+    <UButton type="submit" class="w-full justify-center"> Submit </UButton>
+  </UForm>
+</template>
+
+<script setup lang="ts">
+import { z } from "zod";
+
+const model = defineModel<Code>({ required: true });
+const emit = defineEmits(["submit"]);
+
+const show = ref(false);
+
+const BaseEnum = ["Time based", "Counter based"] as const;
+const schema = z.object({
+  name: z.string().min(1),
+  key: z.string()
+    .regex(/^[A-Z2-7=]+$/, "Wrong key format"),
+  base: z.enum(BaseEnum),
+});
+
+const items = BaseEnum.map((item) => ({
+  label: item,
+  value: item,
+}));
+
+const onSubmit = async () => {
+  emit("submit", model.value);
+};
+</script>
---
+++ src/pages/form/[id].vue
@@ -0,0 +1,20 @@
+<template>
+  <Form v-model="state" @submit="onSubmit" />
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+const id = Number(route.params.id);
+const { getCode, updateCode } = useCodeState();
+const state = ref<Code>({} as Code);
+
+onMounted(async () => {
+  const code = await getCode(id);
+  state.value = code || ({} as Code);
+});
+
+const onSubmit = async () => {
+  await updateCode(id, state.value);
+  navigateTo("/");
+};
+</script>
