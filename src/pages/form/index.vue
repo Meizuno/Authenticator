304465d60777808e5f6d8e15e647efeee7df7244
Yurii Myronov <yuramiron16@gmail.com>
2025-05-26 21:00:28 +0200

    add form page

---
+++ src/pages/form/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <Form v-model="state" @submit="onSubmit" />
+</template>
+
+<script setup lang="ts">
+
+const state = ref({
+  name: "",
+  key: "",
+  base: "Time based",
+} as Code);
+
+const { addCode } = useCodeState();
+
+const onSubmit = async () => {
+  await addCode(state.value);
+  navigateTo("/");
+};
+</script>
