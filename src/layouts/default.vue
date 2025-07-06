a8fc4286bfe92e1e52fb651613e862234797845b
Yurii Myronov <yuramiron16@gmail.com>
2025-07-06 20:42:00 +0200

    update layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,9 +1,9 @@
 <template>
-  <div
+  <UContainer
     class="h-svh grid grid-rows-[auto_1fr] relative overflow-hidden pb-[var(--ion-safe-area-bottom)] pt-[var(--ion-safe-area-top)]"
   >
     <header class="py-4 relative space-y-4">
-      <UContainer class="relative">
+      <div class="relative">
         <UButton
           v-if="$route.name !== 'index'"
           icon="i-material-symbols:arrow-back-ios"
@@ -13,11 +13,11 @@
           @click="$router.back()"
         />
         <div class="flex items-center justify-center gap-2">
-          <img src="assets/logo.ico" alt="Favicon" class="size-5" >
+          <img src="assets/logo.ico" alt="Favicon" class="size-5" />
           <div class="text-xl font-bold text-center">Authenticator</div>
         </div>
-      </UContainer>
-      <UContainer v-if="$route.name === 'index'" as="div">
+      </div>
+      <div v-if="$route.name === 'index'" as="div">
         <UInput
           v-model="search"
           placeholder="Search..."
@@ -36,39 +36,37 @@
             />
           </template>
         </UInput>
-      </UContainer>
+      </div>
     </header>
-    <UContainer as="main" class="px-4 overflow-y-auto overflow-x-hidden">
+    <main class="px-4 overflow-y-auto overflow-x-hidden">
       <slot />
-    </UContainer>
-    <UContainer class="fixed">
-      <UPopover
-        v-if="$route.name === 'index'"
-        :content="{
-          align: 'end',
-          side: 'top',
-          sideOffset: 5,
-        }"
-        class="fixed bottom-6 right-6"
-      >
-        <UButton
-          icon="i-heroicons-plus"
-          variant="solid"
-          class="rounded-full"
-          size="xl"
-          :ui="{ leadingIcon: 'size-9' }"
-        />
+    </main>
+    <UPopover
+      v-if="$route.name === 'index'"
+      :content="{
+        align: 'end',
+        side: 'top',
+        sideOffset: 5,
+      }"
+      class="absolute bottom-6 right-6"
+    >
+      <UButton
+        icon="i-heroicons-plus"
+        variant="solid"
+        class="rounded-full"
+        size="xl"
+        :ui="{ leadingIcon: 'size-9' }"
+      />
 
-        <template #content>
-          <UNavigationMenu
-            orientation="vertical"
-            :items="items"
-            class="text-xl"
-          />
-        </template>
-      </UPopover>
-    </UContainer>
-  </div>
+      <template #content>
+        <UNavigationMenu
+          orientation="vertical"
+          :items="items"
+          class="text-xl"
+        />
+      </template>
+    </UPopover>
+  </UContainer>
 </template>
 
 <script setup lang="ts">
